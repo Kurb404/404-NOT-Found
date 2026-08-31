c670c44f39c4d15cd04f04d6eb697fd20846c967
Kurb404 <aziz401@pm.me>
2026-08-31 16:31:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+fetch('games.json')
+  .then(response => response.json()) 
+  .then(games => {
+    
+    // Sort the entire list of games alphabetically by title
+    games.sort((a, b) => a.title.localeCompare(b.title));
+    
+    // Loop through the newly sorted list
+    games.forEach(game => {
+      
+      const targetGridId = `grid-${game.tier}`;
+      const gridContainer = document.querySelector(`#${targetGridId}`);
+      
+      if (gridContainer) {
+        const gameDiv = document.createElement('div');
+        gameDiv.textContent = game.title;
+        gridContainer.appendChild(gameDiv);
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Error loading the games:', error);
+  });
---
+++ start_server.bat
@@ -0,0 +1,2 @@
+cd /D "C:\Documents\Projects\404 NOT Found"
+python -m http.server 8000
